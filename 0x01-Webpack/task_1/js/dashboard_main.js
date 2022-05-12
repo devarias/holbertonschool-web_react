f33e097aa0a544adf63dc8132a0496d2bfe7f921
David Arias <gedafu2005@gmail.com>
2022-05-12 08:40:28 -0500

    0x01. Webpack

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,19 @@
-import $ from "jquery";
-import _ from 'lodash';
+import $ from 'jquery';
+const _ = require('lodash');
 
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the students</p>");
-$('body').append("<button>Click here to get started</button>");
-$('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+$.when( $.ready ).then(function() {
 
-let aux = 0;
+let counter = 0;
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+};
 
-const updateCounter = () => {
-  aux++;
-  $('#count').html(`${aux} clicks on the button`);
-}
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append($('<p>').attr('id', 'count'));
+$('body').append('<p>Copyright - Holberton School</p>');
 
 $('button').on('click', _.debounce(updateCounter, 500));
+});
--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,31 +1,33 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'production',
+	performance: {
+    maxAssetSize: 100000,
   },
-  module: {
+	module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
-      },
-    ]
-  }
-};
+			{
+				test: /\.css$/, use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true, // webpack@1.x
+							disable: true, // webpack@2.x and newer
+						},
+					},
+				],
+			}
+    ],
+	},
+}
